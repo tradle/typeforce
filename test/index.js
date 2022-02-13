97151a87438b08fa5ecba0be528eeac847d09365
Martin Heidegger <martin.heidegger@gmail.com>
2022-02-14 04:35:35 +0900

    refactor tests: removing overzealous double check of compile and assert method.

--- test/index.js
+++ test/index.js
@@ -5,56 +5,46 @@ const fixtures = require('./fixtures')
 const TYPES = require('./types')
 const VALUES = require('./values')
 
+function testValid ({ typeDescription, compiled }, valid) {
+  const value = VALUES[valid.valueId] || valid.value
+  const valueDescription = JSON.stringify(value)
+
+  tape('passes ' + typeDescription + ' with ' + valueDescription, function (t) {
+    t.plan(3)
+    t.doesNotThrow(function () { typeforce.assert(compiled, value, valid.strict) })
+    typeforceAsync.assert(compiled, value, valid.strict, t.ifErr)
+    t.equal(typeforce.match(compiled, value, valid.strict), true)
+  })
+}
+function testInvalid ({ typeDescription, compiled }, invalid) {
+  if (!invalid.exception) throw new TypeError('Expected exception')
+  const value = VALUES[invalid.valueId] || invalid.value
+  const valueDescription = JSON.stringify(value)
+
+  tape('throws "' + invalid.exception + '" for type ' + typeDescription + ' with value of ' + valueDescription, function (t) {
+    t.plan(5)
+
+    t.throws(function () {
+      typeforce.assert(compiled, value, invalid.strict)
+    }, new RegExp(invalid.exception))
+    typeforceAsync.assert(compiled, value, invalid.strict, (err) => {
+      t.ok(err)
+      t.throws(function () { throw err }, new RegExp(invalid.exception))
+    })
+    t.equal(typeforce.match(compiled, value, invalid.strict), false)
+    t.throws(function () { throw typeforce.match.error }, new RegExp(invalid.exception))
+  })
+}
+
 fixtures.forEach(function (fixture) {
   const type = TYPES[fixture.typeId] || fixture.type
-  const typeDescription = JSON.stringify(type)
-
-  fixture.valid.forEach(valid => {
-    const value = VALUES[valid.valueId] || valid.value
-    const valueDescription = JSON.stringify(value)
-    const compiled = typeforce.compile(type)
-  
-    tape('passes ' + typeDescription + ' with ' + valueDescription, function (t) {
-      t.plan(6)
-      t.doesNotThrow(function () { typeforce.assert(type, value, valid.strict) })
-      typeforceAsync.assert(type, value, valid.strict, t.ifErr)
-      t.equal(typeforce.match(type, value, valid.strict), true)
-  
-      t.doesNotThrow(function () { typeforce.assert(compiled, value, valid.strict) })
-      typeforceAsync.assert(compiled, value, valid.strict, t.ifErr)
-      t.equal(typeforce.match(compiled, value, valid.strict), true)
-    })
-  })
-  fixture.invalid.forEach(invalid => {
-    if (!invalid.exception) throw new TypeError('Expected exception')
-    const value = VALUES[invalid.valueId] || invalid.value
-    const valueDescription = JSON.stringify(value)
-    const compiled = typeforce.compile(type)
-
-    tape('throws "' + invalid.exception + '" for type ' + typeDescription + ' with value of ' + valueDescription, function (t) {
-      t.plan(10)
-
-      t.throws(function () {
-        typeforce.assert(type, value, invalid.strict)
-      }, new RegExp(invalid.exception))
-      typeforceAsync.assert(type, value, invalid.strict, (err) => {
-        t.ok(err)
-        t.throws(function () { throw err }, new RegExp(invalid.exception))
-      })
-      t.equal(typeforce.match(type, value, invalid.strict), false)
-      t.throws(function () { throw typeforce.match.error }, new RegExp(invalid.exception))
-
-      t.throws(function () {
-        typeforce.assert(compiled, value, invalid.strict)
-      }, new RegExp(invalid.exception))
-      typeforceAsync.assert(compiled, value, invalid.strict, (err) => {
-        t.ok(err)
-        t.throws(function () { throw err }, new RegExp(invalid.exception))
-      })
-      t.equal(typeforce.match(compiled, value, invalid.strict), false)
-      t.throws(function () { throw typeforce.match.error }, new RegExp(invalid.exception))
-    })
-  })
+  const description = {
+    type: type,
+    typeDescription: JSON.stringify(type),
+    compiled: typeforce.compile(type)
+  }
+  fixture.valid.forEach(valid => testValid(description, valid))
+  fixture.invalid.forEach(invalid => testInvalid(description, invalid))
 })
 
 const err = new typeforce.TfTypeError('mytype')
