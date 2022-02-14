77660a141e6d3e3880091c0b38f9597fb354c310
Martin Heidegger <martin.heidegger@gmail.com>
2022-02-14 11:28:56 +0900

    fix: removing '' type because it is hard to upgrade, has no test coverage and no clear use.

--- native.js
+++ native.js
@@ -5,8 +5,7 @@ const types = {
   Nil: function (value) { return value === undefined || value === null },
   Number: function (value) { return typeof value === 'number' },
   Object: function (value) { return typeof value === 'object' },
-  String: function (value) { return typeof value === 'string' },
-  '': function () { return true }
+  String: function (value) { return typeof value === 'string' }
 }
 
 // TODO: deprecate
